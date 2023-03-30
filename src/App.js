f052663b3fff7b349e2afb7bda5f3650de4c359f
Fathima <fathimaas91@gmail.com>
2023-03-30 13:06:16 +0200

    added git repository URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Fathima and is{" "}
+        <a
+          href="https://github.com/fathimaanooj/weather-react-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
