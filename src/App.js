3051cb382d6f2034a350625e5847ea7fe51f0938
Fathima <fathimaas91@gmail.com>
2023-03-30 13:41:11 +0200

    added bootstarp

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Fathima and is{" "}
-        <a
-          href="https://github.com/fathimaanooj/weather-react-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Fathima and is{" "}
+          <a href="https://github.com/fathimaanooj/weather-react-app">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
